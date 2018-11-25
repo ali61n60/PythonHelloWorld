399d746ab5fe8a60cd0550d6d6b5917cc9bb80f6
Ali <ali62n62@yahoo.com>
2018-11-25 20:12:51 +0330

    installing packages

--- PythonHelloWorld/PythonHelloWorld.py
+++ PythonHelloWorld/PythonHelloWorld.py
@@ -1,1 +1,10 @@
-print("This line will be printed.")
+import sys 
+from math import cos, radians
+
+def make_dot_string(x):
+    return ' ' * int(20 * cos(radians(x)) + 20) + 'o'
+
+
+for i in range(0,360,12) :
+     s = make_dot_string(i)
+     print(s)
