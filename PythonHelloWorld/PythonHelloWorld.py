db3669e14b0101929781b8e11145f55e5b2eaf79
ali2 <ali62n62@yahoo.com>
2018-11-27 15:34:31 +0330

    6/9/97

--- PythonHelloWorld/PythonHelloWorld.py
+++ PythonHelloWorld/PythonHelloWorld.py
@@ -1,10 +1,21 @@
 import sys 
 from math import cos, radians
 
-def make_dot_string(x):
-    return ' ' * int(20 * cos(radians(x)) + 20) + 'o'
+#def make_dot_string(x):
+#    return ' ' * int(20 * cos(radians(x)) + 20) + 'o'
 
 
-for i in range(0,360,12) :
-     s = make_dot_string(i)
-     print(s)
+#for i in range(0,360,12) :
+#     s = make_dot_string(i)
+#     print(s)
+
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass()
+
+print(myobjectx.variable)
+myobjectx.function()
